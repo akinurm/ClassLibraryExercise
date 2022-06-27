7b3759718a180e3f47caaaae5dc0ce69bfa099ae
Erkki Rasi <erkkirasi@gmail.com>
2022-06-27 14:01:30 +0300

    lisätty alustava versio metodista, joka tarkistaa onko työskentely ajoissa

--- ClassLibraryExercise/Class1.cs
+++ ClassLibraryExercise/Class1.cs
@@ -4,9 +4,26 @@ namespace ClassLibraryExercise
 {
     public class Class1
     {
-        /*Metodi, joka tarkistaa onko annettu päivämäärä tulevaisuudessa ja palauttaa true mikäli 
-         * päivämäärä on tulevaisuudessa ja false mikäli päivämäärä on menneisyydessä*/
-        // TESTI!!
+        /*- Metodi, joka tarkistaa onko annettu päivämäärä tulevaisuudessa ja palauttaa true mikäli 
+         * päivämäärä on tulevaisuudessa ja false mikäli päivämäärä on menneisyydessä
+         
+        - Metodi, joka kertoo opiskelun aloitusajan ja arvioidun opiskeluajan perusteella onko opiskelu myöhässä vai ei. 
+
+         */
+
+        public static bool OnkoAjoissa(DateTime aloitusaika, double arvioituOpiskeluaika)
+        {
+            double kulunutAika = (DateTime.Now.Date - aloitusaika.Date).Days * 24;
+            if (kulunutAika < arvioituOpiskeluaika)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         public static bool OnkoTulevaisuudesta(DateTime paiva)
         {
          if (paiva>DateTime.Now)
