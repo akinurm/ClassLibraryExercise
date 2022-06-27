ecfcf1ae961a3bedcc93a05712b2f4a92b0c2619
ak.nur <aki.nurm@gmail.com>
2022-06-27 13:42:07 +0300

    toinen

--- ClassLibraryExercise/Class1.cs
+++ ClassLibraryExercise/Class1.cs
@@ -4,5 +4,25 @@ namespace ClassLibraryExercise
 {
     public class Class1
     {
+        /*Metodi, joka tarkistaa onko annettu päivämäärä tulevaisuudessa ja palauttaa true mikäli 
+         * päivämäärä on tulevaisuudessa ja false mikäli päivämäärä on menneisyydessä*/
+
+        public static bool OnkoTulevaisuudesta(DateTime paiva)
+        {
+         if (paiva>DateTime.Now)
+            {
+                return true;
+            }
+         else if (paiva < DateTime.Now)
+            {
+                return false;
+            }
+         else
+            {
+                return false;
+            }
+        }
+
+
     }
 }
