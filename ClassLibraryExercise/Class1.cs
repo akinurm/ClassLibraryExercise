8eeb1f0af3685215a7f3a73abe888d257e0402a2
Erkki Rasi <erkkirasi@gmail.com>
2022-06-27 15:54:21 +0300

    Korjattu OnkoAjoissa()-metodia

--- ClassLibraryExercise/Class1.cs
+++ ClassLibraryExercise/Class1.cs
@@ -4,26 +4,37 @@ namespace ClassLibraryExercise
 {
     public class Class1
     {
-        /*- Metodi, joka tarkistaa onko annettu päivämäärä tulevaisuudessa ja palauttaa true mikäli 
-         * päivämäärä on tulevaisuudessa ja false mikäli päivämäärä on menneisyydessä
-         
-        - Metodi, joka kertoo opiskelun aloitusajan ja arvioidun opiskeluajan perusteella onko opiskelu myöhässä vai ei. 
 
-         */
-
+        //Metodi kertoo opiskelun aloitusajan ja arvioidun opiskeluajan perusteella onko opiskelu myöhässä vai ei
         public static bool OnkoAjoissa(DateTime aloitusaika, double arvioituOpiskeluaika)
         {
-            double kulunutAika = (DateTime.Now.Date - aloitusaika.Date).Days * 24;
-            if (kulunutAika < arvioituOpiskeluaika)
+            double verrokki = 0;
+            
+            if (aloitusaika.Date == DateTime.Now.Date)
+            {
+                //ei käytetä TimeSpania
+                verrokki = DateTime.Now.Hour - aloitusaika.Hour;
+            }
+
+            else
+            {
+                //käytetään timespania
+                TimeSpan kulunutAika = DateTime.Now - aloitusaika;
+                verrokki = kulunutAika.TotalHours;
+            }
+            
+            if (verrokki < arvioituOpiskeluaika)
             {
                 return true;
             }
+
             else
             {
                 return false;
             }
         }
 
+        //Metodi palauttaa true jos päivämäärä on tulevaisuudessa ja false mikäli päivämäärä on menneisyydessä
         public static bool OnkoTulevaisuudesta(DateTime paiva)
         {
          if (paiva>DateTime.Now)
